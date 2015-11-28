358135208c321b48b56f40a5d05e57c9fa693d7b
Elliot Penson <elliotpenson@gmail.com>
2015-11-27 22:24:46 -0500

    Make the lexer output token type and value

--- nameless/lexer.py
+++ nameless/lexer.py
@@ -8,8 +8,22 @@ lexer.py
 """
 
 import string
+from collections import namedtuple
+
+PUNCTUATION = [u'λ', '.', '(', ')']
+WHITESPACE = list(string.whitespace)
+
+Token = namedtuple('Token', ['type', 'value'])
+
 
 class Lexer(object):
+    """An iterator that splits lambda calculus source code into Tokens.
+
+    Attributes:
+        source (str): Lambda calculus source code
+        size (int): The number of characters in the source
+        position (int): Current index in the source
+    """
 
     def __init__(self, source):
         self.source = source
@@ -20,31 +34,27 @@ class Lexer(object):
         return self
 
     def next(self):
-        self.clear_whitespace()
-        if self.position < self.size:
-            current = self.source[self.position]
-            if Lexer.terminal_char(current):
-                self.position += 1
-                return current
-            else:
-                return self.symbol()
-        else:
+        """Returns the next lexeme as a Token object."""
+        self._clear_whitespace()
+        if self.position > self.size:
             raise StopIteration()
-
-    def symbol(self):
-        contents = ''
-        for i in range(self.position, self.size):
-            current = self.source[self.position]
-            if Lexer.terminal_char(current) or current.isspace():
-                break
-            contents += current
+        elif self.position == self.size:
             self.position += 1
-        return contents
-
-    def clear_whitespace(self):
-        while self.position < self.size and self.source[self.position].isspace():
+            return Token('EOF', None)
+        elif self.source[self.position] in PUNCTUATION:
+            char = self.source[self.position]
             self.position += 1
+            return Token(char, None)
+        else:
+            symbol = ''
+            while (self.position < self.size and
+                   not self.source[self.position] in PUNCTUATION + WHITESPACE):
+                symbol += self.source[self.position]
+                self.position += 1
+            return Token('SYMBOL', symbol)
 
-    @staticmethod
-    def terminal_char(char):
-        return char in [u'λ', '.', '(', ')']
+    def _clear_whitespace(self):
+        """Advances position past any whitespace."""
+        while (self.position < self.size and
+               self.source[self.position] in string.whitespace):
+            self.position += 1
