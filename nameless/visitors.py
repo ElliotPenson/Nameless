9351f8e82ad9dd93809967d0d75aefbeba9599a2
Elliot Penson <elliotpenson@gmail.com>
2015-12-17 20:10:59 -0600

    Implement alpha conversion functionality

--- nameless/visitors.py
+++ nameless/visitors.py
@@ -29,3 +29,38 @@ class FreeVariables(ast.NodeVisitor):
     def visit_Abstraction(self, node):
         """FV(λx.e) = FV(e) - {x}"""
         return self.visit(node.expression) - self.visit(node.parameter)
+
+class AlphaConversion(ast.NodeVisitor):
+    """Nondestructively substitutes all occurances of a particular variable
+    for an arbitrary expression.
+
+    Attributes:
+        to_return (Variable): Instance whose name attribute must match the
+            variable that's being replaced
+        replacement (Expression): Object inserted into the visited AST
+    """
+
+    def __init__(self, to_replace, replacement):
+        self.to_replace = to_replace
+        self.replacement = replacement
+
+    def visit_Variable(self, node):
+        """If the appropriate variable name is found, replace it."""
+        if node.name == self.to_replace.name:
+            return self.replacement
+        else:
+            return Variable(node.name)
+
+    def visit_Application(self, node):
+        """Returns a new Application after visiting both application
+        expressions.
+        """
+        return Application(self.visit(node.left_expression),
+                           self.visit(node.right_expression))
+
+    def visit_Abstraction(self, node):
+        """Returns a new Abstraction after visiting both the parameter and
+        body.
+        """
+        return Abstraction(self.visit(node.parameter),
+                           self.visit(node.body))
