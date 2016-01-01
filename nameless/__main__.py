e58b7dcfd44704af5c0331a8f49c6e9fc03b92a3
Elliot Penson <elliotpenson@gmail.com>
2016-01-01 14:35:38 -0500

    Wire lexing, parsing, and reduction functionality into the main function

--- nameless/__main__.py
+++ nameless/__main__.py
@@ -7,6 +7,32 @@ main.py
 @author ejnp
 """
 
+from lexer import Lexer
+from parser import Parser, ParserError
+from visitors import BetaReduction
+
+
+def interpret(input_string, print_reductions=False):
+    """Performs normal order reduction on the given string lambda calculus
+    expression. Returns the expression's normal form if it exists.
+    """
+    lexer = Lexer(input_string)
+    try:
+        ast = Parser(lexer).parse()
+    except ParserError as discrepancy:
+        print 'ParseError: ' + discrepancy.message
+        return None
+    normal_form = False
+    while not normal_form:
+        reducer = BetaReduction()
+        reduced_ast = reducer.visit(ast)
+        normal_form = not reducer.reduced
+        if print_reductions:
+            print unicode(ast)
+        ast = reduced_ast
+    return unicode(ast)
+
+
 def main():
     """Begins an interactive lambda calculus interpreter"""
     print "nameless!\nType 'quit' to exit."
@@ -15,7 +41,8 @@ def main():
         if read == 'quit':
             break
         if read != '':
-            print read
+            interpret(read, print_reductions=True)
+
 
 if __name__ == '__main__':
     main()
