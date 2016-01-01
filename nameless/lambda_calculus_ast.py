db4387ade0de75c903e6dc159e51bc991ec432ba
Elliot Penson <elliotpenson@gmail.com>
2016-01-01 13:48:05 -0500

    Move the Printer functionality into special __str__() methods in the ast

--- nameless/lambda_calculus_ast.py
+++ nameless/lambda_calculus_ast.py
@@ -26,6 +26,9 @@ class Variable(Expression):
     def __init__(self, name):
         self.name = name
 
+    def __str__(self):
+        return self.name
+
     def children(self):
         return []
 
@@ -42,6 +45,10 @@ class Application(Expression):
         self.left_expression = left_expression
         self.right_expression = right_expression
 
+    def __str__(self):
+        return u'({} {})'.format(self.left_expression,
+                                 self.right_expression)
+
     def children(self):
         return [self.left_expression, self.right_expression]
 
@@ -57,6 +64,10 @@ class Abstraction(Expression):
     def __init__(self, parameter, body):
         self.parameter = parameter
         self.body = body
+
+    def __str__(self):
+        return u'λ{}.{}'.format(self.parameter,
+                                self.body)
 
     def children(self):
         return [self.parameter, self.body]
